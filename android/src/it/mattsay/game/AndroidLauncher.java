33527865e9d8eb89d411a00409beb7f10bad0347
BuildTools <unconfigured@null.spigotmc.org>
2017-01-28 13:12:41 +0100

    Initial commmit!

---
+++ android/src/it/mattsay/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package it.mattsay.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import it.mattsay.game.SpaceStorm;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SpaceStorm(), config);
+	}
+}
